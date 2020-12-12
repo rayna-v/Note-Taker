b39600da7c2297845559dcf47662fbaf19c9f73a
Jambi 46.2 <69945554+rayna-v@users.noreply.github.com>
2020-12-11 19:46:52 -0700

    working on app.post

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const db = require('./db/db.json');
 const fs = require('fs');
 const { json } = require('express');
-
+// const outputPath = path.join(db_DIR, 'db.json')
+// const render = require("./public/assets/js/index");
 
 // Sets up the Express App
 
@@ -15,17 +16,25 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const newNotes = [];
 
+function writeToFile(fileName, newNotes) {
+    let fileContent = render(newNotes);
+    fs.writeFile(db, fileContent, (err) => {
+        if (err) throw err;
+        console.log("Success")
+    })
+}
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 app.get('/api/notes', (req, res) => {
-    console.log(db);
+    // console.log(db);
     fs.readFile("./db/db.json", 'utf8', (err, data) => {
         if (err) console.log(err);
-        console.log(data);
+        // console.log(data);
         const parsed = JSON.parse(data);
         res.send(parsed);
     })
@@ -35,7 +44,12 @@ app.get('/api/notes', (req, res) => {
 app.post("/api/notes", (req, res) => {
 
     const newNote = req.body;
-    console.log(newNote)
+    console.log(newNote);
+    newNotes.push(newNote);
+    console.log(newNotes);
+    res.json(newNotes);
+    const fileName = `db.json`;
+    writeToFile(fileName, newNotes)
 });
 
 
