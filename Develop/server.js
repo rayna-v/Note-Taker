eb75f3b0e7c2c8cccabec4349db0f399da3f3da3
Jambi 46.2 <69945554+rayna-v@users.noreply.github.com>
2020-12-13 17:26:34 -0700

    refactored and added comments

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+// requiring necessary modules
 const express = require('express');
 const path = require('path');
 const db = require('./db/db.json');
@@ -14,62 +15,63 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// array of objects containing notes from json file
 const newNotes = db;
 
+// writes the notes to the db.json file
 function writeToFile(newNotes) {
-    // let fileContent = render(newNotes);
     fs.writeFile('./db/db.json', newNotes, (err) => {
         if (err) throw err;
         console.log("Success")
     })
 }
+//CREATE 
+// function to create new notes and write to db.json file
+app.post("/api/notes", (req, res) => {
+    req.body.id = cuid();
+    const newNote = req.body;
+    newNotes.push(newNote);
+    console.log(newNotes);
+    writeToFile(JSON.stringify(newNotes));
+    res.json(newNotes);
+});
 
+// READ 
+// function to view index.html
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
+// function to view notes.html
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-
+// function to view api/notes from db.json file
 app.get('/api/notes', (req, res) => {
-
     fs.readFile("./db/db.json", 'utf8', (err, data) => {
         if (err) console.log(err);
         const parsed = JSON.parse(data);
         res.send(parsed);
     })
-
 });
 
-app.put(`/api/notes/:id`, (req, res) => {
+// UPDATE 
+// function to update notes
+// app.put(`/api/notes/:id`, (req, res) => {
 
-    console.log("updating note")
-});
-app.post("/api/notes", (req, res) => {
-    req.body.id = cuid();
-    const newNote = req.body;
+//     console.log("updating note")
+// });
 
-    newNotes.push(newNote);
-    console.log(newNotes);
-    writeToFile(JSON.stringify(newNotes));
-    res.json(newNotes);
-
-});
-
+// DELETE
+// function to delete notes 
 app.delete(`/api/notes/:id`, (req, res) => {
     console.log("deleting");
     fs.readFile("./db/db.json", 'utf8', (err, data) => {
-        // console.log(req.params.id)
         let notDeleted = JSON.parse(data).filter((val) => {
-
-            // console.log('val.id: ' + );
             console.log(`Request ID: ` + req.params.id + `|| Note ID: ` + val.id)
             return val.id !== req.params.id;
-
         })
-        // const parsed = JSON.parse(notDeleted);
         if (err) console.log(err);
-        console.log(notDeleted)
         res.send(notDeleted);
         writeToFile(JSON.stringify(notDeleted));
     })
 
 })
+
+// function to bind/listen the connections on local host and port
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
